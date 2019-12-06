3f39b06d653a1e9f42d456490372302d8c37c250
Josef Wittmann <josefwittmann42@gmail.com>
2019-12-06 17:23:20 +0100

    Fix: Searching active after exit game

--- src/reducers/lobby.tsx
+++ src/reducers/lobby.tsx
@@ -1,4 +1,9 @@
-import { LOAD_LOBBY, START_SEARCHING, STOP_SEARCHING } from "../constants";
+import {
+  LOAD_LOBBY,
+  START_SEARCHING,
+  STOP_SEARCHING,
+  START_GAME
+} from "../constants";
 import { Action } from "./ActionInterface";
 
 export interface LobbyData {
@@ -23,6 +28,7 @@ export const lobby = (state: LobbyData = initialState, action: Action) => {
       return { ...state, ...action.payload };
     case START_SEARCHING:
       return { ...state, isSearching: true };
+    case START_GAME:
     case STOP_SEARCHING:
       return { ...state, isSearching: false };
     default:
